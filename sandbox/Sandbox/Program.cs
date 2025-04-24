115b339fb85cc50c6eb76c165b6029468fa6aa5c
kaitlinheusner <kheusner@byui.edu>
2025-04-24 11:04:11 -0600

    Prove Complete

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // This is a program to compute the area of a circle. 
+        // Get the radius of a circle from the user 
+
+        Console.Write("What is the radius of a circle? ");
+        //Write line would make the input go on a new line
+        string text = Console.ReadLine();
+        double radius = double.Parse(text);
+        
+        //Compute the are of the circle
+        double area = Math.PI * radius * radius;
+
+
+        // int x = 3;
+        // string s = "goodbye";
+        // float f = 3.14F;
+        // double d = 5.21; 
+        // //double is 64 bits and float is 34 bits
+        // /* */
+        // long n = 25;
+
+        Console.WriteLine($"Area of the circle: {area}");
     }
 }
