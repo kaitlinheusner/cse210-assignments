9970e9d2ea63ffc68722453f53a1f04cc88838cc
kaitlinheusner <kheusner@byui.edu>
2025-05-14 15:41:13 -0600

    Program Updated and Completed

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -13,8 +13,11 @@ class Program
 
     static void Menu()
     {
-        Console.WriteLine("Welcome to the Journal Program!");
+        Journal journal = new Journal();
         int menuChoice = 0;
+
+        Console.WriteLine("Welcome to the Journal Program!");
+
         while (menuChoice != 5)
         {
             Console.Write("Please select one of the following choices: ");
@@ -26,29 +29,44 @@ class Program
             "3. Load \r\n" +
             "4. Save \r\n" +
             "5. Quit \r\n";
+
             Console.WriteLine(choices);
 
             Console.Write("What would you like to do? ");
             Console.Write("");
             menuChoice = int.Parse(Console.ReadLine()); 
-
             
             if (menuChoice == 1)
             {
-                var (prompt, response) = Entry.WriteEntry();
-                Journal.AddEntry(prompt, response);
+                Entry entry = new Entry();
+                entry.WriteEntry();
+                journal.AddEntry(entry);
             }
 
             else if (menuChoice == 2)
             {
-                Journal.Display();
+                journal.Display();
+            }
+
+            else if (menuChoice == 3)
+            {
+                Console.Write("What is the filename? ");
+                string fileName = Console.ReadLine();
+                journal._fileName = fileName;
+                journal.LoadJournal();
             }
 
             else if (menuChoice == 4)
             {
                 Console.Write("What is the filename? ");
-                string filename = Console.ReadLine();
-                Journal.SaveJournal(filename);
+                string fileName = Console.ReadLine();
+                journal._fileName = fileName;
+                journal.SaveJournal();
+            }
+
+            else if (menuChoice == 5)
+            {
+                break;
             }
         }
         
@@ -57,52 +75,76 @@ class Program
 
 public class Journal
 {
-    private static List<(string prompt, string response)> entries = new List<(string, string)> ();
+    private List<Entry> _entries = new List<Entry> ();
+    public string _fileName;
     
-    public static void AddEntry(string prompt, string response)
+    public void AddEntry(Entry entry)
     {
-        entries.Add((prompt, response));
+        _entries.Add(entry);
     }
-    public static void Display()
+
+    public void Display()
     {
-        string dataText = DateTime.Now.ToShortDateString();
-        foreach (var (prompt, response) in entries)
+        foreach (Entry entry in _entries)
         {
-            Console.WriteLine($"Date: {dataText} - Prompt {prompt}");
-            Console.WriteLine(response);
+            entry.Display();
         }
     }
 
-    public static void SaveJournal(string filename)
+    public void SaveJournal()
     {
-        string dataText = DateTime.Now.ToShortDateString(); 
-
-        using (StreamWriter outputFile = new StreamWriter(filename))
+        using (StreamWriter outputFile = new StreamWriter(_fileName, false))
         {
-            foreach (var (prompt, response) in entries)
+            foreach (Entry entry in _entries)
             {
-                outputFile.WriteLine($"Date: {dataText} - Prompt {prompt}");
-                outputFile.WriteLine(response);
+                outputFile.WriteLine($"{entry._date}|{entry._prompt}|{entry._response}");
             }
         }
     }
 
-    public static void LoadJournal(string filename)
+    public void LoadJournal()
     {
+        _entries.Clear();        
+        string[] journalEntries = System.IO.File.ReadAllLines(_fileName);
 
+        foreach (string journalEntry in journalEntries)
+        {
+            string[] entryParts = journalEntry.Split("|");
+            string date = entryParts[0];
+            string prompt = entryParts[1];
+            string response = entryParts[2];
+
+            Entry entry = new Entry();
+            entry._date = date;
+            entry._prompt = prompt;
+            entry._response = response;
+
+            _entries.Add(entry);
+        }
     }
 }
 
 public class Entry
 {
-    public static (string prompt, string response) WriteEntry()
-    {
-        string prompt = PromptGenerator.PromptMenu();
-        Console.WriteLine(prompt);
-        Console.Write("> ");
-        string response = Console.ReadLine();
+    public string _prompt;
+    public string _response;
+    public string _date;
 
-        return (prompt, response);
+    public void WriteEntry()
+    {
+        _prompt = PromptGenerator.PromptMenu();
+        Console.WriteLine(_prompt);
+        Console.Write("> ");
+        _response = Console.ReadLine();
+        Console.WriteLine("");
+        _date = DateTime.Now.ToShortDateString();
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Date: {_date} - Prompt: {_prompt}");
+        Console.WriteLine(_response);
+        Console.WriteLine("");
     }
 }
 
@@ -110,28 +152,37 @@ public class PromptGenerator
 {
     static List<string> _familyPrompts = new List<string>()
         {
-            "What is a fun memory you have with your family."
+            "What is a fun memory you have with your family? ",
+            "How many siblings do you have? ", 
+            "Where are your parents from? ",
         };
 
     static List<string> _schoolPrompts = new List<string>()
         {
-            "What did you enjoy about school?"
+            "What do you enjoy about school? ",
+            "How have your assignments been going?", 
+            "What is your favorite class? "
         };
 
-    static List<string> _weatherPrompts = new List<string>()
+    static List<string> _naturePrompts = new List<string>()
         {
-            "What is your favorite weather? "
+            "What is your favorite weather? ",
+            "What did you enjoy about the outdoors today?",
+            "Do you like flowers? (If yes, do you have a favorite?)", 
+
         };
 
     static List<string> _lifePrompts = new List<string>()
     {
-        "How have things been going recently? "
+        "How have things been going recently? ",
+        "Who was the most interesting person I interacted with today?",
+        "How did I see the hand of the Lord in my life today? "
     };
 
     public static string PromptMenu()
     {
         Console.WriteLine("");
-        Console.Write("Please select what you would like to write about: ");
+        Console.Write("Please select a prompt suggestion for what you would like to write about: ");
 
         string promptChoices = 
         "\r\n" +
@@ -161,11 +212,11 @@ public class PromptGenerator
 
         else if (promptChoice == 3)
         {
-            int index = rnd.Next(_weatherPrompts.Count);
-            prompt = _weatherPrompts[index];
+            int index = rnd.Next(_naturePrompts.Count);
+            prompt = _naturePrompts[index];
         }
 
-        else if (promptChoice ==4 )
+        else if (promptChoice == 4)
         {
             int index = rnd.Next(_lifePrompts.Count);
             prompt = _lifePrompts[index];
