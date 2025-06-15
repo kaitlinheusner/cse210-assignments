5a22b332d671008edebf2c75906b40981b037862
kaitlinheusner <kheusner@byui.edu>
2025-06-15 17:00:03 -0600

    Prove Completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square1 = new Square("Green", 4);
+        shapes.Add(square1);
+
+        Rectangle rectangle1 = new Rectangle("Orange", 9, 4);
+        shapes.Add(rectangle1);
+
+        Circle circle1 = new Circle("Purple", 4);
+        shapes.Add(circle1);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} {s} has an area of {area}");
+        }
     }
 }
